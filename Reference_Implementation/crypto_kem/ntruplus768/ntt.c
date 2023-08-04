88c6d560b6ecc0b68777ba991dfa3b5cafedefef
yoswuk <81409818+yoswuk@users.noreply.github.com>
2023-08-04 15:54:37 +0900

    Update ntt.c

--- Reference_Implementation/crypto_kem/ntruplus768/ntt.c
+++ Reference_Implementation/crypto_kem/ntruplus768/ntt.c
@@ -26,7 +26,7 @@ void ntt(int16_t b[NTRUPLUS_N], const int16_t a[NTRUPLUS_N])
 	{
 		zeta1 = zetas[k++];
 		zeta2 = zetas[k++];
-
+/*
 		for(int i = start; i < start + 128; i++)
 		{
 			t1 = fqmul(zeta1, b[i + 128]);
@@ -41,6 +41,17 @@ void ntt(int16_t b[NTRUPLUS_N], const int16_t a[NTRUPLUS_N])
 			b[i + 128] = fqred16(b[i] + t3);
 			b[i    ] = fqred16(b[i] + t1);
 		}
+*/
+		for(int i = start; i < start + 128; i++)
+		{
+			t1 = fqmul(zeta1, b[i +   step]);
+			t2 = fqmul(zeta2, b[i + 2*step]);
+			t3 = fqmul(2571, t1 - t2);
+
+			b[i + 2*step] = fqred16(b[i] - t1 - t3);
+			b[i +   step] = fqred16(b[i] - t2 + t3);
+			b[i         ] = fqred16(b[i] + t1 + t2);
+		}		
 	}
 
 	for(int step = 64; step >= 2; step >>= 1)
