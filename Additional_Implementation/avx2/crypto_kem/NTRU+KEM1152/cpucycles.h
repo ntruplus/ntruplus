70eb191a192bf14c9d8a85e6dbd78a240ce52990
yoswuk <yoswuk@korea.ac.kr>
2024-07-25 15:42:49 +0900

    EOF

--- Additional_Implementation/avx2/crypto_kem/NTRU+KEM1152/cpucycles.h
+++ Additional_Implementation/avx2/crypto_kem/NTRU+KEM1152/cpucycles.h
@@ -6,24 +6,24 @@
 #ifdef USE_RDPMC  /* Needs echo 2 > /sys/devices/cpu/rdpmc */
 
 static inline uint64_t cpucycles(void) {
-  const uint32_t ecx = (1U << 30) + 1;
-  uint64_t result;
-
-  __asm__ volatile ("rdpmc; shlq $32,%%rdx; orq %%rdx,%%rax"
-    : "=a" (result) : "c" (ecx) : "rdx");
-
-  return result;
+	const uint32_t ecx = (1U << 30) + 1;
+	uint64_t result;
+	
+	__asm__ volatile ("rdpmc; shlq $32,%%rdx; orq %%rdx,%%rax"
+	: "=a" (result) : "c" (ecx) : "rdx");
+	
+	return result;
 }
 
 #else
 
 static inline uint64_t cpucycles(void) {
-  uint64_t result;
-
-  __asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
-    : "=a" (result) : : "%rdx");
-
-  return result;
+	uint64_t result;
+	
+	__asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
+	: "=a" (result) : : "%rdx");
+	
+	return result;
 }
 
 #endif
