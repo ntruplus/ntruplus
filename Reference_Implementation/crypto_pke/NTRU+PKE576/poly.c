bf442dbcee0593fdb055ae32b93e3f7a591120dc
yoswuk <yoswuk@korea.ac.kr>
2024-07-25 17:38:25 +0900

    EOF

--- Reference_Implementation/crypto_pke/NTRU+PKE576/poly.c
+++ Reference_Implementation/crypto_pke/NTRU+PKE576/poly.c
@@ -86,12 +86,12 @@ void poly_frombytes(poly *r, const uint8_t a[NTRUPLUS_POLYBYTES])
 **************************************************/
 static uint32_t load32_littleendian(const uint8_t x[4])
 {
-  uint32_t r;
-  r  = (uint32_t)x[0];
-  r |= (uint32_t)x[1] << 8;
-  r |= (uint32_t)x[2] << 16;
-  r |= (uint32_t)x[3] << 24;
-  return r;
+	uint32_t r;
+	r  = (uint32_t)x[0];
+	r |= (uint32_t)x[1] << 8;
+	r |= (uint32_t)x[2] << 16;
+	r |= (uint32_t)x[3] << 24;
+	return r;
 }
 
 /*************************************************
