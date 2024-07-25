4a74ef11074f6f5f1cb02cbffe0975d6e29ee4ea
yoswuk <81409818+yoswuk@users.noreply.github.com>
2024-07-25 16:40:34 +0900

    EOF

--- Reference_Implementation/crypto_pke/NTRU+PKE864/reduce.c
+++ Reference_Implementation/crypto_pke/NTRU+PKE864/reduce.c
@@ -15,11 +15,11 @@
 **************************************************/
 int16_t montgomery_reduce(int32_t a)
 {
-  int16_t t;
-
-  t = (int16_t)a*QINV;
-  t = (a - (int32_t)t*NTRUPLUS_Q) >> 16;
-  return t;
+	int16_t t;
+	
+	t = (int16_t)a*QINV;
+	t = (a - (int32_t)t*NTRUPLUS_Q) >> 16;
+	return t;
 }
 
 /*************************************************
@@ -34,10 +34,10 @@ int16_t montgomery_reduce(int32_t a)
 **************************************************/
 int16_t barrett_reduce(int16_t a)
 {
-  int16_t t;
-  const int16_t v = ((1<<26) + NTRUPLUS_Q/2)/NTRUPLUS_Q;
-
-  t  = ((int32_t)v*a + (1<<25)) >> 26;
-  t *= NTRUPLUS_Q;
-  return a - t;
+	int16_t t;
+	const int16_t v = ((1<<26) + NTRUPLUS_Q/2)/NTRUPLUS_Q;
+	
+	t  = ((int32_t)v*a + (1<<25)) >> 26;
+	t *= NTRUPLUS_Q;
+	return a - t;
 }
