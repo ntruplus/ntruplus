a91e927dcb5957f8be3e6dfb3ddb8e5bf34c8d35
yoswuk <81409818+yoswuk@users.noreply.github.com>
2024-08-29 13:41:02 +0900

    Update kem.c

--- Reference_Implementation/crypto_kem/NTRU+KEM1152/kem.c
+++ Reference_Implementation/crypto_kem/NTRU+KEM1152/kem.c
@@ -40,13 +40,14 @@ int crypto_kem_keypair(unsigned char *pk, unsigned char *sk)
 		f.coeffs[0] += 1;
 		poly_ntt(&f,&f);
 		r = poly_baseinv(&finv, &f);
+		if(r) continue;
 		
 		poly_cbd1(&g, buf + NTRUPLUS_N/4); 
 		poly_triple(&g);
 		poly_ntt(&g,&g);
 		
 		poly_basemul(&h,&g,&finv);
-		r |= poly_baseinv(&hinv,&h);
+		r = poly_baseinv(&hinv,&h);
 	} while(r);
 	
 	//pk
