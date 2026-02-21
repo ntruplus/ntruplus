5cd3acf1ab8a724c612f0082884a8fa35b501a00
yoswuk <yoswuk@korea.ac.kr>
2026-02-22 01:22:48 +0900

    improve fqinv_batch for aarch64

--- Additional_Implementation/aarch64/NTRU+1152/poly.c
+++ Additional_Implementation/aarch64/NTRU+1152/poly.c
@@ -5,7 +5,7 @@ static const int16_t consts[] __attribute__((aligned(16))) = {
     0x0d81, 0x4bd4, 0xcd7f, 0xff6d, 0xfa8f, 0xf9dd, 0xc5d5, 0x0000
 };
 
-static inline int16x8_t fqmul_neon(int16x8_t x, int16x8_t y, int16x8_t con)
+static inline int16x8_t fqmul(int16x8_t x, int16x8_t y, int16x8_t con)
 {
     int32x4_t l, h;
     int16x8_t t;
@@ -22,30 +22,30 @@ static inline int16x8_t fqmul_neon(int16x8_t x, int16x8_t y, int16x8_t con)
     return vuzp2q_s16(vreinterpretq_s16_s32(l), vreinterpretq_s16_s32(h));
 }
 
-static inline int16x8_t fqinv_neon(int16x8_t a, int16x8_t con)
+static inline int16x8_t fqinv(int16x8_t a, int16x8_t con)
 {
     int16x8_t t1, t2, t3;
 
-    t1 = fqmul_neon(a,  a,  con); // 10
-    t2 = fqmul_neon(t1, t1, con); // 100
-    t2 = fqmul_neon(t2, t2, con); // 1000
-    t3 = fqmul_neon(t2, t2, con); // 10000
+    t1 = fqmul(a,  a,  con); // 10
+    t2 = fqmul(t1, t1, con); // 100
+    t2 = fqmul(t2, t2, con); // 1000
+    t3 = fqmul(t2, t2, con); // 10000
 
-    t1 = fqmul_neon(t1, t2, con); // 1010
+    t1 = fqmul(t1, t2, con); // 1010
 
-    t2 = fqmul_neon(t1, t3, con); // 11010
-    t2 = fqmul_neon(t2, t2, con); // 110100
-    t2 = fqmul_neon(t2, a,  con); // 110101
+    t2 = fqmul(t1, t3, con); // 11010
+    t2 = fqmul(t2, t2, con); // 110100
+    t2 = fqmul(t2, a,  con); // 110101
 
-    t1 = fqmul_neon(t1, t2, con); // 111111
+    t1 = fqmul(t1, t2, con); // 111111
 
-    t2 = fqmul_neon(t2, t2, con); // 1101010
-    t2 = fqmul_neon(t2, t2, con); // 11010100
-    t2 = fqmul_neon(t2, t2, con); // 110101000
-    t2 = fqmul_neon(t2, t2, con); // 1101010000
-    t2 = fqmul_neon(t2, t2, con); // 11010100000
-    t2 = fqmul_neon(t2, t2, con); // 110101000000
-    t2 = fqmul_neon(t2, t1, con); // 110101111111
+    t2 = fqmul(t2, t2, con); // 1101010
+    t2 = fqmul(t2, t2, con); // 11010100
+    t2 = fqmul(t2, t2, con); // 110101000
+    t2 = fqmul(t2, t2, con); // 1101010000
+    t2 = fqmul(t2, t2, con); // 11010100000
+    t2 = fqmul(t2, t2, con); // 110101000000
+    t2 = fqmul(t2, t1, con); // 110101111111
 
     t1   = vqrdmulhq_laneq_s16(t2, con, 6);
     t2 = vmulq_laneq_s16(t2, con, 5);
@@ -54,28 +54,53 @@ static inline int16x8_t fqinv_neon(int16x8_t a, int16x8_t con)
     return t2;
 }
 
-static inline int poly_fqinv_batch(int16x8_t r[36], int16x8_t con)
+static inline int poly_fqinv_batch(int16x8_t* r, int16x8_t con)
 {
-    int16x8_t c[36];
-    int16x8_t inv;
+    const int chunk = NTRUPLUS_N / 96;
+    const int off0 = 0 * chunk;
+    const int off1 = 1 * chunk;
+    const int off2 = 2 * chunk;
 
-    c[0] = r[0];
+    int16x8_t t[NTRUPLUS_N / 32];
 
-    for (int i = 1; i < 36; i++)
-        c[i] = fqmul_neon(c[i - 1], r[i], con);
+    t[off0] = r[off0];
+    t[off1] = r[off1];
+    t[off2] = r[off2];
 
-    if (!vminvq_u16(vreinterpretq_u16_s16(c[35]))) return 1;
-
-    inv = fqinv_neon(c[35], con);
-
-    for (int i = 35; i > 0; i--)
+    for (int i = 1; i < chunk; i++)
     {
-        int16x8_t ri = r[i];
-        r[i] = fqmul_neon(c[i - 1], inv, con);
-        inv  = fqmul_neon(inv, ri, con);
+        t[off0 + i] = fqmul(t[off0 + i - 1], r[off0 + i], con);
+        t[off1 + i] = fqmul(t[off1 + i - 1], r[off1 + i], con);
+        t[off2 + i] = fqmul(t[off2 + i - 1], r[off2 + i], con);
     }
 
-    r[0] = inv;
+    if (!vminvq_u16(vreinterpretq_u16_s16(t[off0 + chunk - 1])) &&
+        !vminvq_u16(vreinterpretq_u16_s16(t[off1 + chunk - 1])) &&
+        !vminvq_u16(vreinterpretq_u16_s16(t[off2 + chunk - 1])))
+        return 1;
+
+    int16x8_t inv0 = fqinv(t[off0 + chunk - 1], con);
+    int16x8_t inv1 = fqinv(t[off1 + chunk - 1], con);
+    int16x8_t inv2 = fqinv(t[off2 + chunk - 1], con);
+
+    for (int i = chunk - 1; i > 0; i--)
+    {
+        int16x8_t tmp0 = r[off0 + i];
+        int16x8_t tmp1 = r[off1 + i];
+        int16x8_t tmp2 = r[off2 + i];
+
+        r[off0 + i] = fqmul(t[off0 + i - 1], inv0, con);
+        r[off1 + i] = fqmul(t[off1 + i - 1], inv1, con);
+        r[off2 + i] = fqmul(t[off2 + i - 1], inv2, con);
+
+        inv0 = fqmul(inv0, tmp0, con);
+        inv1 = fqmul(inv1, tmp1, con);
+        inv2 = fqmul(inv2, tmp2, con);
+    }
+
+    r[off0] = inv0;
+    r[off1] = inv1;
+    r[off2] = inv2;
 
     return 0;
 }
@@ -98,10 +123,10 @@ static inline void poly_baseinv_2(poly *r, int16x8_t *den, int16x8_t con)
         int16x8_t r2 = vld1q_s16(rp + offset + 16);
         int16x8_t r3 = vld1q_s16(rp + offset + 24);
 
-        r0 = fqmul_neon(r0, pden, con);
-        r1 = fqmul_neon(r1, mden, con);
-        r2 = fqmul_neon(r2, pden, con);
-        r3 = fqmul_neon(r3, mden, con);
+        r0 = fqmul(r0, pden, con);
+        r1 = fqmul(r1, mden, con);
+        r2 = fqmul(r2, pden, con);
+        r3 = fqmul(r3, mden, con);
 
         vst1q_s16(rp + offset +  0, r0);
         vst1q_s16(rp + offset +  8, r1);
