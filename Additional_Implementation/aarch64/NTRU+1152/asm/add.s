2172a4526b96956ab58db7ac999570cabea9d122
yoswuk <yoswuk@korea.ac.kr>
2026-07-16 06:24:02 +0900

    Preserve AArch64 SIMD callee-saved registers

--- Additional_Implementation/aarch64/NTRU+1152/asm/add.s
+++ Additional_Implementation/aarch64/NTRU+1152/asm/add.s
@@ -9,34 +9,35 @@ _poly_sub:
 
     mov counter, #2304
 
+    # v8-v15 are callee-saved; use v0-v7 and v16-v31 only.
 .align 2
 _loop_sub:
     ld1 {v0.8h - v3.8h},  [src1], #64
     ld1 {v4.8h - v7.8h},  [src1], #64
-    ld1 {v8.8h - v11.8h}, [src1], #64
+    ld1 {v16.8h - v19.8h}, [src1], #64
 
-    ld1 {v12.8h - v15.8h}, [src2], #64
-    ld1 {v16.8h - v19.8h}, [src2], #64
     ld1 {v20.8h - v23.8h}, [src2], #64
+    ld1 {v24.8h - v27.8h}, [src2], #64
+    ld1 {v28.8h - v31.8h}, [src2], #64
 
-    sub v0.8h,  v0.8h,  v12.8h
-    sub v1.8h,  v1.8h,  v13.8h
-    sub v2.8h,  v2.8h,  v14.8h
-    sub v3.8h,  v3.8h,  v15.8h
+    sub v0.8h,  v0.8h,  v20.8h
+    sub v1.8h,  v1.8h,  v21.8h
+    sub v2.8h,  v2.8h,  v22.8h
+    sub v3.8h,  v3.8h,  v23.8h
     st1 {v0.8h, v1.8h}, [dst], #32
-    sub v4.8h,  v4.8h,  v16.8h
-    sub v5.8h,  v5.8h,  v17.8h
+    sub v4.8h,  v4.8h,  v24.8h
+    sub v5.8h,  v5.8h,  v25.8h
     st1 {v2.8h, v3.8h}, [dst], #32
-    sub v6.8h,  v6.8h,  v18.8h
-    sub v7.8h,  v7.8h,  v19.8h
+    sub v6.8h,  v6.8h,  v26.8h
+    sub v7.8h,  v7.8h,  v27.8h
     st1 {v4.8h, v5.8h}, [dst], #32
-    sub v8.8h,  v8.8h,  v20.8h
-    sub v9.8h,  v9.8h,  v21.8h
+    sub v16.8h, v16.8h, v28.8h
+    sub v17.8h, v17.8h, v29.8h
     st1 {v6.8h, v7.8h}, [dst], #32
-    sub v10.8h, v10.8h, v22.8h
-    sub v11.8h, v11.8h, v23.8h
-    st1 {v8.8h,  v9.8h},  [dst], #32
-    st1 {v10.8h, v11.8h}, [dst], #32
+    sub v18.8h, v18.8h, v30.8h
+    sub v19.8h, v19.8h, v31.8h
+    st1 {v16.8h, v17.8h}, [dst], #32
+    st1 {v18.8h, v19.8h}, [dst], #32
 
     subs counter, counter, #192
     b.ne _loop_sub
@@ -61,11 +62,12 @@ _poly_triple:
     movi v0.8h, #3
     mov counter, #2304
 
+    # v8-v15 are callee-saved; use v0-v4 and v16-v23 only.
 .align 2
 _loop_triple:
-    ld1 {v1.8h - v4.8h},   [src], #64
-    ld1 {v5.8h - v8.8h},   [src], #64
-    ld1 {v9.8h - v12.8h},  [src], #64
+    ld1 {v1.8h - v4.8h},    [src], #64
+    ld1 {v16.8h - v19.8h},  [src], #64
+    ld1 {v20.8h - v23.8h},  [src], #64
 
     mul v1.8h,  v1.8h,  v0.8h
     mul v2.8h,  v2.8h,  v0.8h
@@ -74,19 +76,19 @@ _loop_triple:
     mul v4.8h,  v4.8h,  v0.8h
     st1 {v3.8h, v4.8h}, [dst], #32
 
-    mul v5.8h,  v5.8h,  v0.8h
-    mul v6.8h,  v6.8h,  v0.8h
-    st1 {v5.8h, v6.8h}, [dst], #32
-    mul v7.8h,  v7.8h,  v0.8h
-    mul v8.8h,  v8.8h,  v0.8h
-    st1 {v7.8h, v8.8h}, [dst], #32
+    mul v16.8h, v16.8h, v0.8h
+    mul v17.8h, v17.8h, v0.8h
+    st1 {v16.8h, v17.8h}, [dst], #32
+    mul v18.8h, v18.8h, v0.8h
+    mul v19.8h, v19.8h, v0.8h
+    st1 {v18.8h, v19.8h}, [dst], #32
 
-    mul v9.8h,   v9.8h,   v0.8h
-    mul v10.8h, v10.8h,   v0.8h
-    st1 {v9.8h, v10.8h},  [dst], #32
-    mul v11.8h, v11.8h,   v0.8h
-    mul v12.8h, v12.8h,   v0.8h
-    st1 {v11.8h, v12.8h}, [dst], #32
+    mul v20.8h, v20.8h, v0.8h
+    mul v21.8h, v21.8h, v0.8h
+    st1 {v20.8h, v21.8h}, [dst], #32
+    mul v22.8h, v22.8h, v0.8h
+    mul v23.8h, v23.8h, v0.8h
+    st1 {v22.8h, v23.8h}, [dst], #32
 
     subs counter, counter, #192
     b.ne _loop_triple
